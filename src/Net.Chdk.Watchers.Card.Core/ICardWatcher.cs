72e41d8c515c35b0711df40119d3be6278d29740
Dmitry Shechtman <damnian@gmail.com>
2017-04-21 20:25:48 +0300

    Add solution

---
+++ src/Net.Chdk.Watchers.Card.Core/ICardWatcher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Net.Chdk.Watchers.Card
+{
+    public interface ICardWatcher : IDisposable
+    {
+        void Initialize();
+        void Start();
+        void Stop();
+        event EventHandler<string> CardAdded;
+        event EventHandler<string> CardRemoved;
+    }
+}
