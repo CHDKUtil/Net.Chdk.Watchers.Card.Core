4e6ca47f36a8e4b5a0423b609c31b98e99afdd75
Dmitry Shechtman <damnian@gmail.com>
2017-05-09 01:15:04 +0300

    Card -> Volume

--- src/Net.Chdk.Watchers.Volume.Core/Properties/AssemblyInfo.cs
+++ src/Net.Chdk.Watchers.Volume.Core/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Net.Chdk.Watchers.Card.Core")]
+[assembly: AssemblyTitle("Net.Chdk.Watchers.Volume.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
